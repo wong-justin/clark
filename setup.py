f384f85424be0a2ddfafc1140be219a402ac000b
wong-justin <jkwongfl@yahoo.com>
2023-07-09 20:49:31 -0400

    rename again, this time to a non-hyphenated module name
    findings:
    topmost setup package name can be nearly anything; its unrelated
    src dir doesnt have to be src/something
      just ./something is ok
      but cant be something-hyphenated/
      at least for the sake of console_scripts entry point

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setuptools.setup(
     description='A TUI for media playback and timestamping, using MPV.',
     packages=setuptools.find_packages(),
     install_requires=['blessed', 'docopt', 'python-mpv'],
-    entry_points={'console_scripts': ['clark=clark-mpv:run_cli']},
+    entry_points={'console_scripts': ['clark=clark.main:run_cli']},
     python_requires='>=3.7',
 )
